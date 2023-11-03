11fcc2f7b8d86f380c8a9f035777fc6a9f598acd
Caroline Ho <cho@bayviewglen.ca>
2023-11-02 22:41:24 -0400

    Lesson 4 practice

--- Unit 2/Lesson 4/practice.py
+++ Unit 2/Lesson 4/practice.py
@@ -30,3 +30,36 @@ def volume_of_a_cube(len):
 height = 4
 print(volume_of_a_cube(height))
 
+
+#Given 2 strings, a and b, return a new string made of the first char of a and the last char of b
+def last_chars(a, b):
+    if all(isinstance(x, str) for x in [a, b]):
+        a = a[:2:1]
+        length = len(b) + 1
+        end = length-1
+        b = b[end:length:1]
+        return a+b
+
+a = "Hello"
+b = "Bai"
+print(last_chars(a,b))
+
+
+#Given a string of any length
+def last_two(str):
+    length = len(str)
+    front = str[:2]
+    end = str[3:length+1]
+    return end+front
+
+str = "Hello"
+print(last_two(str))
+
+#4
+def extraFront(str):
+    length = len(str)
+    front = str[:2]
+    return front + front + front
+
+str = "Hello"
+print(extraFront(str))
