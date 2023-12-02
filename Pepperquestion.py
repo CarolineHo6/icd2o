eef364f7ef108c531d51125d0b4c3a10876cb838
Caroline Ho <cho@bayviewglen.ca>
2023-12-02 16:57:55 -0500

    pepper problem

--- Pepperquestion.py
+++ Pepperquestion.py
@@ -8,3 +8,23 @@ thai = 75000
 habanero = 125000
 
 
+for i in range(n):
+    types = input("Enter the name of the pepper Ron has used: ")
+    if types == "poblano".lower:
+        i + 1
+    elif types == "mirasol".lower:
+        i + 1
+    elif types == "serrano".lower:
+        i + 1
+    elif types == "cayenne".lower:
+        i + 1
+    elif types == "thai".lower:
+        i + 1
+    elif types == "habanero".lower:
+        i + 1
+    else:
+        break
+
+total = 0
+t = total + types
+
